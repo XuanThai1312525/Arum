58049e2f49630d179f9d89ab003abab7b4f43def
trinh.ho2 <trinh.ho2@mservice.com.vn>
2021-06-04 01:42:07 +0700

    #Notification handler

--- Arum/Helpers/Utils/NotificationManager.swift
+++ Arum/Helpers/Utils/NotificationManager.swift
@@ -27,7 +27,7 @@ class NotificationManager: NSObject {
             NotificationManager.userInfo = userInfo
         }
         
-        if UserSession.roleSubject.value == .logged , let userInfo = NotificationManager.userInfo{
+        if let userInfo = NotificationManager.userInfo{
             handleNotification(userInfo: userInfo)
             NotificationManager.userInfo = nil
         }
@@ -39,18 +39,37 @@ class NotificationManager: NSObject {
     
     static func handleNotification(userInfo: [String: Any]){
         guard let aps = userInfo["aps"] as? [String: Any],
-              let data = aps["data"] as? [String: Any],
-              let pushType = data["pushType"] as? Int
+              let click_action = aps["category"] as? String
         else {
             return
         }
         
-        switch pushType {
-            case 1:
+        guard let nav = AppDelegate.shared.window.rootViewController as? BaseNavigationVC else {
+            return
+        }
+        
+        if let vc = nav.viewControllers.last(where: {$0.isKind(of: ARWebContentViewController.self)}) as? ARWebContentViewController {
+            vc.urlString = click_action
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                nav.rootViewController?.pop(to: vc)
+            }
+        } else {
+            //Block code
+//            let vc = UIStoryboard.main.instantiateViewController(withIdentifier: "ARWebContentViewController") as! ARWebContentViewController
+//            vc.urlString = click_action
+//            nav.pushViewController(vc, animated: true)
+//
+            
+            let vc = UIStoryboard.main.instantiateViewController(withIdentifier: "ARWebContentViewController") as! ARWebContentViewController
+            vc.urlString = "https://aleum.kr"
+            nav.pushViewController(vc, animated: true)
+            
+            //Please don't open click action force. because cannot touch go back
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                vc.urlString = click_action
                 
-            break
-        default:
-            break
+            }
+            
         }
         
     }
