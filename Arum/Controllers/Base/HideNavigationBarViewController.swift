19867459bff9eb621f41c083ce61d9b3d14407e7
trinh.ho2 <trinh.ho2@mservice.com.vn>
2021-06-07 17:11:02 +0700

    #Hide navigation bar

--- Arum/Controllers/Base/HideNavigationBarViewController.swift
+++ Arum/Controllers/Base/HideNavigationBarViewController.swift
@@ -15,6 +15,16 @@ class HideNavigationBarViewController: BaseViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func setupNavigationBar() {
         super.setupNavigationBar()
         hideNavigationBar()
